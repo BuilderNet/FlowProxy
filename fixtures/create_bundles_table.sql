88ac54e6bbe520d45698ccb714be6fc4abb14c25
Lorenzo <lorenzo.feroleto.dev@gmail.com>
2025-09-29 11:54:16 +0200

    feat(indexer): clickhouse support for bundles and transactions (#10)
    This PR introduces an optional Clickhouse indexer configurable from CLI.
    It tracks full bundles received from the user endpoints and writes them
    to the database.
    
    The goal of this is to being able to replicate real flow in a testing
    environment, as such it provides also testing code from converting
    clickhouse data to `RawBundle`s.

---
+++ fixtures/create_bundles_table.sql
@@ -0,0 +1,57 @@
+-- DDL to create a ClickHouse table for storing Ethereum bundles data.
+CREATE TABLE bundles (
+  `time` DateTime64(6),
+  `timestamp` DateTime64(6) ALIAS time,
+  `transactions.hash` Array(FixedString(32)),
+  `transactions.from` Array(FixedString(20)),
+  `transactions.nonce` Array(UInt64),
+  `transactions.r` Array(UInt256),
+  `transactions.s` Array(UInt256),
+  `transactions.v` Array(UInt8),
+  `transactions.to` Array(Nullable(FixedString(20))),
+  `transactions.gas` Array(UInt64),
+  `transactions.type` Array(UInt8),
+  `transactions.input` Array(String),
+  `transactions.value` Array(UInt256),
+  `transactions.gasPrice` Array(Nullable(UInt128)),
+  `transactions.maxFeePerGas` Array(Nullable(UInt128)),
+  `transactions.maxPriorityFeePerGas` Array(Nullable(UInt128)),
+  `transactions.accessList` Array(Nullable(String)),
+  `transactions.authorizationList` Array(Nullable(String)),
+
+  `block_number` Nullable(UInt64),
+  `min_timestamp` Nullable(UInt64),
+  `max_timestamp` Nullable(UInt64),
+
+  `reverting_tx_hashes` Array(FixedString(32)),
+  `dropping_tx_hashes` Array(FixedString(32)),
+  `refund_tx_hashes` Array(FixedString(32)),
+
+  `hash` FixedString(32),
+  `internal_uuid` Nullable(UUID),
+  `replacement_uuid` Nullable(UUID),
+  `replacement_nonce` Nullable(UInt64),
+  `refund_percent` Nullable(UInt8),
+  `refund_recipient` Nullable(FixedString(20)),
+  `signer_address` Nullable(FixedString(20)),
+  `refund_identity` Nullable(FixedString(20)),
+
+  `builder_name` LowCardinality(String),
+
+  INDEX from_bloom_filter `transactions.from` TYPE bloom_filter GRANULARITY 10,
+  INDEX transactions_hash_bloom_filter `transactions.hash` TYPE bloom_filter GRANULARITY 10,
+  INDEX hash_bloom_filter hash TYPE bloom_filter GRANULARITY 10,
+  INDEX internal_uuid_bloom_filter internal_uuid TYPE bloom_filter GRANULARITY 10,
+  
+  -- For bundles: `internal_uuid` should be set.
+  -- For replacement bundles with transactions: `internal_uuid` and `replacement_uuid` should be set.
+  -- For replacement bundles without transactions (a.k.a. "cancellations"): only `replacement_uuid` should be set.
+  -- So this is the invariant we want to enforce:
+  CONSTRAINT valid_uuid_or_replacement CHECK (internal_uuid IS NOT NULL OR replacement_uuid IS NOT NULL)
+)
+ENGINE = ReplicatedMergeTree('/clickhouse/tables/{uuid}/{shard}', '{replica}')
+PARTITION BY toYYYYMM(time)
+PRIMARY KEY (time)
+ORDER BY (time)
+TTL toDateTime(time) + toIntervalMonth(1) RECOMPRESS CODEC(ZSTD(6))
+SETTINGS index_granularity = 8192;
