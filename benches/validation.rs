ad46232378e8e7f1e9ffb363cfd7b20409a76495
Jonas Bostoen <jonasbostoen@fastmail.com>
2025-10-08 10:10:20 +0200

    fix: fix tests + benches

--- benches/validation.rs
+++ benches/validation.rs
@@ -4,8 +4,9 @@ use alloy_primitives::{keccak256, Address};
 use alloy_signer::SignerSync;
 use alloy_signer_local::PrivateKeySigner;
 use buildernet_orderflow_proxy::{
-    ingress::{maybe_verify_signature, FLASHBOTS_SIGNATURE_HEADER},
-    types::SystemBundle,
+    consts::FLASHBOTS_SIGNATURE_HEADER,
+    ingress::maybe_verify_signature,
+    types::{SystemBundle, UtcInstant},
     utils::testutils::Random,
 };
 use criterion::{criterion_group, criterion_main, BatchSize, BenchmarkId, Criterion, Throughput};
@@ -13,12 +14,11 @@ use hyper::HeaderMap;
 use rand::{rngs::StdRng, Rng, SeedableRng};
 use rbuilder_primitives::serialize::RawBundle;
 use serde_json::json;
-use time::UtcDateTime;
 
 struct RawBundleWithSigner {
     raw_bundle: RawBundle,
     signer: Address,
-    received_at: UtcDateTime,
+    received_at: UtcInstant,
 }
 
 struct SignedRequest {
@@ -31,7 +31,7 @@ impl Random for RawBundleWithSigner {
         Self {
             raw_bundle: RawBundle::random(rng),
             signer: Address::random_with(rng),
-            received_at: UtcDateTime::now(),
+            received_at: UtcInstant::now(),
         }
     }
 }
