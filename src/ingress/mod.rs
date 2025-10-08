d2293c0f41d66f77965b9d965545c882e951d401
Jonas Bostoen <jonasbostoen@fastmail.com>
2025-10-08 14:21:31 +0200

    fix(ingress): set replacement nonce if needed

--- src/ingress/mod.rs
+++ src/ingress/mod.rs
@@ -395,7 +395,7 @@ impl OrderflowIngress {
     async fn on_bundle(
         &self,
         entity: Entity,
-        bundle: RawBundle,
+        mut bundle: RawBundle,
         received_at: UtcDateTime,
     ) -> Result<B256, IngressError> {
         let start = Instant::now();
@@ -413,6 +413,13 @@ impl OrderflowIngress {
         }
 
         self.order_cache.insert(bundle_hash);
+
+        // Set replacement nonce if it is not set. This is needed to decode the replacement data
+        // correctly in [`SystemBundle::try_from_bundle_and_signer`].
+        if bundle.replacement_uuid.is_some() && bundle.replacement_nonce.is_none() {
+            let timestamp = received_at.unix_timestamp_nanos() / 1000;
+            bundle.replacement_nonce = Some(timestamp.try_into().expect("Timestamp too large"));
+        }
 
         // Decode and validate the bundle.
         let bundle = self
