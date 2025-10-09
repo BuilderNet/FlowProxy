673b5d7416edf1d046a421d9dff1a2b8ac434b76
Lorenzo <lorenzo.feroleto.dev@gmail.com>
2025-10-09 18:22:35 +0200

    feat: ensure max file descriptors (#71)
    ~This PR introduces a flag `--file-descriptors <number>` that ensures
    the provided number of file descriptors can be used by the process,
    respecting hard limits, and a capped value of `8192`. If they're already
    higher than the provided value, it is a no-op.~
    
    Uses the `fdlimit` create to bump to the max the values of file
    descriptors for this process.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 use buildernet_orderflow_proxy::{
     cli::OrderflowIngressArgs,
+    init_tracing,
     runner::{CliContext, CliRunner},
 };
 use clap::Parser;
 
 fn main() {
+    let args = OrderflowIngressArgs::parse();
+    init_tracing(args.log_json);
+
     let tokio_runtime = tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .build()
         .expect("failed to create runtime");
 
     let runner = CliRunner::from_runtime(tokio_runtime);
+
     let command =
         |ctx: CliContext| buildernet_orderflow_proxy::run(OrderflowIngressArgs::parse(), ctx);
 
