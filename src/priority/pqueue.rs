a2fe400c1c39a38b5225bed6cc39b89f6c2e8536
Jonas Bostoen <jonasbostoen@fastmail.com>
2025-10-15 16:59:19 +0200

    feat(metrics): add priority queue metrics, bump queue capacities (#91)
    ## Description
    Adds metrics (sampled) for tracking priority queue permits per level.
    May be useful to see when we're hitting limits. Also bumped the avail
    permits for low and medium.

--- src/priority/pqueue.rs
+++ src/priority/pqueue.rs
@@ -2,25 +2,35 @@ use super::Priority;
 use std::sync::Arc;
 use tokio::sync::{oneshot, Semaphore};
 
+const DEFAULT_HIGH_QUEUE_SIZE: usize = Semaphore::MAX_PERMITS;
+const DEFAULT_MEDIUM_QUEUE_SIZE: usize = 50_000;
+const DEFAULT_LOW_QUEUE_SIZE: usize = 5_000;
+
 /// Priority level permits for processing and validating user requests.
 /// See [`Priority`] for details.
 #[derive(Debug)]
 pub struct PriorityQueues {
     /// Permits for [`Priority::High`].
     high: Arc<Semaphore>,
+    high_size: usize,
     /// Permits for [`Priority::Medium`].
     medium: Arc<Semaphore>,
+    medium_size: usize,
     /// Permits for [`Priority::Low`].
     low: Arc<Semaphore>,
+    low_size: usize,
 }
 
 /// Opinionated priority queues defaults.
 impl Default for PriorityQueues {
     fn default() -> Self {
         Self {
-            high: Arc::new(Semaphore::new(Semaphore::MAX_PERMITS)),
-            medium: Arc::new(Semaphore::new(10_000)),
-            low: Arc::new(Semaphore::new(1_000)),
+            high: Arc::new(Semaphore::new(DEFAULT_HIGH_QUEUE_SIZE)),
+            high_size: DEFAULT_HIGH_QUEUE_SIZE,
+            medium: Arc::new(Semaphore::new(DEFAULT_MEDIUM_QUEUE_SIZE)),
+            medium_size: DEFAULT_MEDIUM_QUEUE_SIZE,
+            low: Arc::new(Semaphore::new(DEFAULT_LOW_QUEUE_SIZE)),
+            low_size: DEFAULT_LOW_QUEUE_SIZE,
         }
     }
 }
@@ -30,8 +40,11 @@ impl PriorityQueues {
     pub fn new(high: usize, medium: usize, low: usize) -> Self {
         Self {
             high: Arc::new(Semaphore::new(high)),
+            high_size: high,
             medium: Arc::new(Semaphore::new(medium)),
+            medium_size: medium,
             low: Arc::new(Semaphore::new(low)),
+            low_size: low,
         }
     }
 
@@ -42,6 +55,25 @@ impl PriorityQueues {
             Priority::Medium => &self.medium,
             Priority::Low => &self.low,
         }
+    }
+
+    /// Return the available permits for the given priority.
+    pub fn available_permits_for(&self, priority: Priority) -> usize {
+        self.semaphore_for(priority).available_permits()
+    }
+
+    /// Return the total permits for the given priority.
+    pub fn total_permits_for(&self, priority: Priority) -> usize {
+        match priority {
+            Priority::High => self.high_size,
+            Priority::Medium => self.medium_size,
+            Priority::Low => self.low_size,
+        }
+    }
+
+    /// Return the active permits for the given priority.
+    pub fn active_permits_for(&self, priority: Priority) -> usize {
+        self.total_permits_for(priority) - self.available_permits_for(priority)
     }
 
     /// Spawn the task with given priority.
