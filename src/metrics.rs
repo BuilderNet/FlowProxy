2d69b905e2cedae97bd1c00321cbaf8da5e96be4
thedevbirb <lorenzo.feroleto.dev@gmail.com>
2025-10-20 10:43:42 +0200

    chore(metrics): specify decompressed

--- src/metrics.rs
+++ src/metrics.rs
@@ -70,7 +70,8 @@ mod name {
         pub(crate) const RPC_REQUEST_DURATION: &str = "ingress_rpc_request_duration";
         pub(crate) const VALIDATION_ERRORS: &str = "ingress_validation_errors";
 
-        pub(crate) const REQUEST_BODY_SIZE: &str = "ingress_request_body_size";
+        pub(crate) const REQUEST_BODY_SIZE_DECOMPRESSED: &str =
+            "ingress_request_body_size_decompressed";
         pub(crate) const TXS_PER_BUNDLE: &str = "ingress_txs_per_bundle";
         pub(crate) const TXS_PER_MEV_SHARE_BUNDLE: &str = "ingress_txs_per_mev_share_bundle";
         pub(crate) const TOTAL_EMPTY_BUNDLES: &str = "ingress_total_empty_bundles";
@@ -209,7 +210,10 @@ pub fn describe() {
     );
     describe_counter!(ingress::VALIDATION_ERRORS, "Total number of validation errors");
 
-    describe_histogram!(ingress::REQUEST_BODY_SIZE, "Size of incoming request bodies in bytes");
+    describe_histogram!(
+        ingress::REQUEST_BODY_SIZE_DECOMPRESSED,
+        "Size of incoming request bodies in bytes, decompressed"
+    );
     describe_histogram!(ingress::TXS_PER_BUNDLE, "Number of transactions per bundle");
     describe_histogram!(
         ingress::TXS_PER_MEV_SHARE_BUNDLE,
@@ -364,7 +368,7 @@ pub trait IngressHandlerMetricsExt {
 
     #[inline]
     fn record_request_body_size_bytes(size: usize, method: &'static str) {
-        histogram!(ingress::REQUEST_BODY_SIZE, "handler" => Self::HANDLER, method => "method")
+        histogram!(ingress::REQUEST_BODY_SIZE_DECOMPRESSED, "handler" => Self::HANDLER, method => "method")
             .record(size as f64);
     }
 
