3d4934066370919fd0d8d77dc003c9aafee679e3
Jonas Bostoen <jonasbostoen@fastmail.com>
2025-11-21 16:14:29 +0100

    test: clean up haproxy test

--- tests/network.rs
+++ tests/network.rs
@@ -1,21 +1,12 @@
-use std::{
-    io::Read as _, net::SocketAddr, num::NonZero, path::PathBuf, str::FromStr as _, time::Duration,
-};
-
-use alloy_primitives::Bytes;
-
-mod common;
 use alloy_consensus::TxEnvelope;
 use alloy_eips::Encodable2718 as _;
-use alloy_signer_local::PrivateKeySigner;
+use alloy_primitives::Bytes;
 use common::{spawn_ingress, BuilderReceiver};
-use flowproxy::{
-    cli::OrderflowIngressArgs, statics::LOCAL_PEER_STORE, utils::testutils::Random as _,
-};
+use flowproxy::utils::testutils::Random as _;
 use rbuilder_primitives::serialize::{RawBundle, RawShareBundle};
-use tokio::io::AsyncReadExt;
+use std::time::Duration;
 
-use crate::common::{spawn_haproxy, spawn_ingress_with_args};
+mod common;
 
 /// This tests proper order propagation between 2 proxies.
 #[tokio::test]
@@ -85,106 +76,123 @@ async fn network_e2e_mev_share_bundle() {
     assert_eq!(received, bundle);
 }
 
-/// Generate key material for TLS like this (from the ./testdata/certificates/ directory):
-/// ```bash
-/// openssl req -newkey rsa:2048 -nodes \
-///     -keyout client.key \
-///     -out /tmp/client.csr \
-///     -subj "/CN=client"
-///   openssl x509 -req -days 365 -sha256 \
-///     -in /tmp/client.csr \
-///     -signkey client.key \
-///     -out client.crt
-///   rm /tmp/client.csr
-///
-///   openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-///     -keyout default.key \
-///     -out default.crt \
-///     -config ../openssl.cnf \
-///     -extensions v3_req
-///
-///
-/// cat default.key default.crt > default.pem
-/// ```
-#[tokio::test]
-async fn network_e2e_tls() {
-    let testdata_dir = PathBuf::from(concat!(env!("CARGO_MANIFEST_DIR"), "/tests/testdata"));
-    let cert_dir = testdata_dir.join("certificates");
+/// NOTE: This only works on Linux, because OpenSSL behaves differently on macOS (in a way that
+/// makes this test fail).
+#[cfg(target_os = "linux")]
+mod linux {
+    use std::{net::SocketAddr, num::NonZero, path::PathBuf, str::FromStr as _, time::Duration};
 
-    let signer1 = PrivateKeySigner::random();
-    let signer2 = PrivateKeySigner::random();
+    use alloy_signer_local::PrivateKeySigner;
+    use flowproxy::{
+        cli::OrderflowIngressArgs, statics::LOCAL_PEER_STORE, utils::testutils::Random as _,
+    };
+    use rbuilder_primitives::serialize::RawBundle;
+    use tokio::io::AsyncReadExt as _;
 
-    let mut args = OrderflowIngressArgs::default().disable_builder_hub();
-    args.private_key_pem_file = cert_dir.join("default.key");
-    args.certificate_pem_file = cert_dir.join("default.crt");
-    args.orderflow_signer = Some(signer1);
-    args.client_pool_size = NonZero::new(1).unwrap();
+    use crate::common::{spawn_haproxy, spawn_ingress_with_args, BuilderReceiver};
 
-    let mut args2 = args.clone();
-    args2.private_key_pem_file = cert_dir.join("default.key");
-    args2.certificate_pem_file = cert_dir.join("default.crt");
-    args2.orderflow_signer = Some(signer2.clone());
-    // Listen on port 5552, the TCP receiver
-    args2.system_listen_addr_http = SocketAddr::from_str("127.0.0.1:5542").unwrap();
-    args2.system_listen_addr_tcp = SocketAddr::from_str("127.0.0.1:5552").unwrap();
-    args2.disable_forwarding = true;
+    /// Generate key material for TLS like this (from the ./testdata/certificates/ directory):
+    /// ```bash
+    /// openssl req -newkey rsa:2048 -nodes \
+    ///     -keyout client.key \
+    ///     -out /tmp/client.csr \
+    ///     -subj "/CN=client"
+    ///   openssl x509 -req -days 365 -sha256 \
+    ///     -in /tmp/client.csr \
+    ///     -signkey client.key \
+    ///     -out client.crt
+    ///   rm /tmp/client.csr
+    ///
+    ///   openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    ///     -keyout default.key \
+    ///     -out default.crt \
+    ///     -config ../openssl.cnf \
+    ///     -extensions v3_req
+    ///
+    ///
+    /// cat default.key default.crt > default.pem
+    /// ```
+    #[tokio::test]
+    async fn network_e2e_tls() {
+        let testdata_dir = PathBuf::from(concat!(env!("CARGO_MANIFEST_DIR"), "/tests/testdata"));
+        let cert_dir = testdata_dir.join("certificates");
 
-    let mut builder1 = BuilderReceiver::spawn().await;
-    let mut builder2 = BuilderReceiver::spawn().await;
+        let signer1 = PrivateKeySigner::random();
+        let signer2 = PrivateKeySigner::random();
 
-    args.builder_url = Some(builder1.url());
-    args2.builder_url = Some(builder2.url());
+        let mut args = OrderflowIngressArgs::default().disable_builder_hub();
+        args.private_key_pem_file = cert_dir.join("default.key");
+        args.certificate_pem_file = cert_dir.join("default.crt");
+        args.orderflow_signer = Some(signer1);
+        args.client_pool_size = NonZero::new(1).unwrap();
 
-    let haproxy = spawn_haproxy(&testdata_dir.join("haproxy.cfg"), &cert_dir).await.unwrap();
+        let mut args2 = args.clone();
+        args2.private_key_pem_file = cert_dir.join("default.key");
+        args2.certificate_pem_file = cert_dir.join("default.crt");
+        args2.orderflow_signer = Some(signer2.clone());
+        // Listen on port 5552, the TCP receiver
+        args2.system_listen_addr_http = SocketAddr::from_str("127.0.0.1:5542").unwrap();
+        args2.system_listen_addr_tcp = SocketAddr::from_str("127.0.0.1:5552").unwrap();
+        args2.disable_forwarding = true;
 
-    tokio::time::sleep(Duration::from_secs(1)).await;
+        let mut builder1 = BuilderReceiver::spawn().await;
+        let mut builder2 = BuilderReceiver::spawn().await;
 
-    let client1 = spawn_ingress_with_args(args).await;
-    let client2 = spawn_ingress_with_args(args2).await;
+        args.builder_url = Some(builder1.url());
+        args2.builder_url = Some(builder2.url());
 
-    tokio::time::sleep(Duration::from_secs(1)).await;
+        let haproxy = spawn_haproxy(&testdata_dir.join("haproxy.cfg"), &cert_dir).await.unwrap();
 
-    // Override server instance data
-    let ps = LOCAL_PEER_STORE.clone();
-    ps.builders.entry(signer2.address().to_string()).and_modify(|entry| {
-        // Overwrite the IP address to HAProxy system API
-        entry.ip = format!("127.0.0.1:5544");
-        entry.dns_name = "localhost".to_string();
-        entry.instance.tls_cert = std::fs::read_to_string(cert_dir.join("default.crt")).unwrap();
-    });
+        tokio::time::sleep(Duration::from_secs(1)).await;
 
-    let mut rng = rand::rng();
+        let client1 = spawn_ingress_with_args(args).await;
+        let _client2 = spawn_ingress_with_args(args2).await;
 
-    // Listens on port 5554
+        tokio::time::sleep(Duration::from_secs(1)).await;
 
-    tokio::time::sleep(Duration::from_secs(5)).await;
+        // Override server instance data
+        let ps = LOCAL_PEER_STORE.clone();
+        ps.builders.entry(signer2.address().to_string()).and_modify(|entry| {
+            // Overwrite the IP address to HAProxy system API
+            entry.ip = format!("127.0.0.1:5544");
+            entry.dns_name = "localhost".to_string();
+            entry.instance.tls_cert =
+                std::fs::read_to_string(cert_dir.join("default.crt")).unwrap();
+        });
 
-    let bundle = RawBundle::random(&mut rng);
-    let response = client1.send_bundle(&bundle).await;
-    assert!(response.status().is_success());
+        let mut rng = rand::rng();
 
-    let mut received = builder1.recv::<RawBundle>().await.unwrap();
+        // Listens on port 5554
 
-    assert!(received.metadata.signing_address.is_some());
-    assert!(received.metadata.bundle_hash.is_some());
-    // NOTE: This will have a signing address populated which we reset
-    received.metadata.signing_address = None;
-    received.metadata.bundle_hash = None;
-    assert_eq!(received, bundle);
+        tokio::time::sleep(Duration::from_secs(5)).await;
 
-    let mut received = builder2.recv::<RawBundle>().await.unwrap();
-    assert!(received.metadata.signing_address.is_some());
-    assert!(received.metadata.bundle_hash.is_some());
-    // NOTE: This will have a signing address populated which we reset
-    received.metadata.signing_address = None;
-    received.metadata.bundle_hash = None;
-    assert_eq!(received, bundle);
+        let bundle = RawBundle::random(&mut rng);
+        let response = client1.send_bundle(&bundle).await;
+        assert!(response.status().is_success());
 
-    let mut stdout = String::new();
-    haproxy.stdout(false).read_to_string(&mut stdout).await.unwrap();
-    let mut stderr = String::new();
-    haproxy.stderr(false).read_to_string(&mut stderr).await.unwrap();
+        let mut received = builder1.recv::<RawBundle>().await.unwrap();
 
-    println!("{}", stdout);
-    println!("{}", stderr);
+        assert!(received.metadata.signing_address.is_some());
+        assert!(received.metadata.bundle_hash.is_some());
+        // NOTE: This will have a signing address populated which we reset
+        received.metadata.signing_address = None;
+        received.metadata.bundle_hash = None;
+        assert_eq!(received, bundle);
+
+        let mut received = builder2.recv::<RawBundle>().await.unwrap();
+        assert!(received.metadata.signing_address.is_some());
+        assert!(received.metadata.bundle_hash.is_some());
+        // NOTE: This will have a signing address populated which we reset
+        received.metadata.signing_address = None;
+        received.metadata.bundle_hash = None;
+        assert_eq!(received, bundle);
+
+        let mut stdout = String::new();
+        haproxy.stdout(false).read_to_string(&mut stdout).await.unwrap();
+        let mut stderr = String::new();
+        haproxy.stderr(false).read_to_string(&mut stderr).await.unwrap();
+
+        println!("{}", stdout);
+        println!("{}", stderr);
+    }
 }
